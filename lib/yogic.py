851d12959e839f78b8c234997cff1d155b2952fe
Mick Krippendorf <m.krippendorf@freenet.de>
2020-01-08 02:51:17 +0100

    Version bump 0.8a

--- lib/yogic.py
+++ lib/yogic.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2020 Mick Krippendorf <m.krippendorf@freenet.de>
 
-__version__ = '0.7a'
+__version__ = '0.8a'
 __date__ = '2020-01-01'
 __author__ = 'Mick Krippendorf <m.krippendorf@freenet.de>'
 __license__ = 'MIT'
