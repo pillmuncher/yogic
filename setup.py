ac0cf434f7627fadd4a8fcdd0ec8a26b512520e8
Mick Krippendorf <m.krippendorf@freenet.de>
2021-04-13 21:16:59 +0200

    Some cleanup & version bump.

--- setup.py
+++ setup.py
@@ -11,4 +11,7 @@ __license__ = 'MIT'
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        packages=find_packages(where='src'),
+        package_dir={'':'src'}
+    )
