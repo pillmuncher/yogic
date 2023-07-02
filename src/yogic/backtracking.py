844975f677a28a4db9224883c2a457ec18b47c21
Mick Krippendorf <m.krippendorf@freenet.de>
2023-07-02 23:20:45 +0200

    re-introduced yogic.py and backtracking.py. Also introduced type hints in backtracking.py

---
+++ src/yogic/backtracking.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2021 Mick Krippendorf <m.krippendorf@freenet.de>
+
+from functools import reduce
+from itertools import chain
+from collections.abc import Iterable
+from typing import Callable, Generator, Sequence, TypeVar
+
+
+# A monad for backtracking. Hence it's called the Backtracking Monad.
+# Actually, it's just the Continuation Monad wrapped around the List Monad.
+
+
+# The naming convention follows the mathematical notation. Therefor:
+# pylint: disable=C0103
+
+
+Value = TypeVar('Value')
+
+Result = Iterable[Value]
+Cont = Callable[[Value], Result]
+A = Callable[[Cont], Result]
+MA = Callable[[A], Result]
+MF = Callable[[Value], MA]
+
+
+def bind(ma: MA, mf: MF) -> MA:
+    '''Return the result of applying mf to ma.'''
+    return lambda c: ma(lambda v: mf(v)(c))
+
+
+def unit(v: Value) -> MA:
+    '''Take the single value v into the monad. Represents success.
+    Together with 'then', this makes the monad also a monoid.'''
+    return lambda c: c(v)  # type: ignore
+
+
+def zero(v: Value) -> MA:  # pylint: disable=W0613
+    '''Ignore the value v and return an 'empty' monad. Represents failure.'''
+    return lambda c: ()
+
+
+def no(mf: MF) -> MF:
+    '''Invert the result of a monadic computation, AKA negation as failure.'''
+    def _(v: Value):
+        def __(c: Cont):
+            for result in mf(v)(c):  # pylint: disable=W0612
+                # If at least one solution is found, fail immediately:
+                return zero(v)(c)
+            else:  # pylint: disable=W0120
+                # If no solution is found, succeed:
+                return unit(v)(c)
+        return __
+    return _
+
+
+def then(mf: MF, mg: MF) -> MF:
+    '''Apply two monadic functions mf and mg in sequence.
+    Together with 'unit', this makes the monad also a monoid.'''
+    return lambda v: bind(mf(v), mg)
+
+
+def _alt_from_iterable(mfs: Sequence[MF]) -> MF:
+    '''Find solutions matching any one of mfs.'''
+    return lambda v: lambda c: chain.from_iterable(mf(v)(c) for mf in mfs)
+
+
+def alt(*mfs: MF) -> MF:
+    '''Find solutions matching any one of mfs.'''
+    return _alt_from_iterable(mfs)
+
+alt.from_iterable = _alt_from_iterable  # type: ignore
+
+
+def _seq_from_iterable(mfs: Sequence[MF]) -> MF:
+    '''Find solutions matching all mfs.'''
+    return reduce(then, mfs, unit)  # type: ignore
+
+
+def seq(*mfs: MF) -> MF:
+    '''Find solutions matching all mfs.'''
+    return _seq_from_iterable(mfs)
+
+seq.from_iterable = _seq_from_iterable  # type: ignore
+
+
+def run(ma: MA) -> Result:
+    '''Start the monadic computation of ma.'''
+    return ma(lambda v: (yield v))  # type: ignore
---
+++ src/yogic/yogic.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2021 Mick Krippendorf <m.krippendorf@freenet.de>
+
+from collections import namedtuple, ChainMap
+from collections.abc import Mapping
+from functools import wraps
+from itertools import count
+
+from .backtracking import unit, zero, seq, run
+
+
+# Variable objects to be bound to values in a monadic computation:
+Variable = namedtuple('Variable', 'id')
+_var_counter = count()
+
+
+def var():
+    '''Helper function to create Variables.'''
+    return Variable(next(_var_counter))
+
+
+class Subst(ChainMap):
+    '''An environment that maps Variables to the values they are bound to during
+    a monadic computation.'''
+
+    def chase(self, obj):
+        '''Chase down Variable bindings.'''
+        match obj:
+            case Variable() as variable:
+                if variable in self:  # pylint: disable=R1705
+                    return self.chase(self[variable])
+                else:
+                    return variable
+            case list() | tuple() as sequence:
+                return type(sequence)(self.chase(each) for each in sequence)
+            case _:
+                return obj
+
+    @property
+    class proxy(Mapping):  # pylint: disable=C0103
+        '''A proxy interface to Subst.'''
+        def __init__(self, subst):
+            self._subst = subst
+        def __getitem__(self, variable):
+            return self._subst.chase(variable)
+        def __iter__(self):
+            return iter(self._subst)
+        def __len__(self):
+            return len(self._subst)
+
+
+def _unify(this, that):  # pylint: disable=R0911
+    # Unify two objects in a Subst:
+    match this, that:
+        case Variable(), object():
+            # Bind a Variable to another object:
+            if this is that:  # pylint: disable=R1705
+                # a Variable, already bound or not, is always bound to itself:
+                return unit
+            else:
+                # bind this to that while creating a new choice point:
+                return lambda subst: unit(subst.new_child({this: that}))
+        case object(), Variable():
+            # Same as above, but with swapped arguments:
+            return _unify(that, this)  # pylint: disable=W1114
+        case list() | tuple(), list() | tuple():
+            # Recursively unify two lists or tuples:
+            if type(this) == type(that) and len(this) == len(that):  # pylint: disable=R1705,C0123
+                return seq.from_iterable(map(unify, this, that))
+            else:
+                return zero
+        case _:
+            # Unify other objects only if they're equal:
+            if this == that:  # pylint: disable=R1705
+                return unit
+            else:
+                return zero
+
+
+# Public interface to _unify:
+def unify(this, that):
+    '''Unify 'this' and 'that'.
+    If at least one is an unbound Variable, bind it to the other object.
+    If both are either lists or tuples, try to unify them recursively.
+    Otherwise, unify them if they are equal.'''
+    return lambda subst: _unify(subst.chase(this), subst.chase(that))(subst)  # pylint: disable=E1102,W0108
+
+
+def predicate(func):
+    '''Helper decorator for yogic functions.'''
+    @wraps(func)
+    def _(*args, **kwargs):
+        return lambda subst: func(*args, **kwargs)(subst)  # pylint: disable=W0108
+    return _
+
+
+def resolve(goal):
+    '''Start the logical resolution of 'goal'. Return all solutions.'''
+    return (subst.proxy for subst in run(goal(Subst())))
