376edda8e54a7ddec8fca7b2a5a7873e62e544c9
Mick Krippendorf <m.krippendorf@freenet.de>
2023-07-02 23:34:56 +0200

    Fixed wrong type hints and changed MA/MF to Ma/Mf.

--- src/yogic/backtracking.py
+++ src/yogic/backtracking.py
@@ -18,28 +18,27 @@ Value = TypeVar('Value')
 
 Result = Iterable[Value]
 Cont = Callable[[Value], Result]
-A = Callable[[Cont], Result]
-MA = Callable[[A], Result]
-MF = Callable[[Value], MA]
+Ma = Callable[[Cont], Result]
+Mf = Callable[[Value], Ma]
 
 
-def bind(ma: MA, mf: MF) -> MA:
+def bind(ma: Ma, mf: Mf) -> Ma:
     '''Return the result of applying mf to ma.'''
     return lambda c: ma(lambda v: mf(v)(c))
 
 
-def unit(v: Value) -> MA:
+def unit(v: Value) -> Ma:
     '''Take the single value v into the monad. Represents success.
     Together with 'then', this makes the monad also a monoid.'''
-    return lambda c: c(v)  # type: ignore
+    return lambda c: c(v)
 
 
-def zero(v: Value) -> MA:  # pylint: disable=W0613
+def zero(v: Value) -> Ma:  # pylint: disable=W0613
     '''Ignore the value v and return an 'empty' monad. Represents failure.'''
     return lambda c: ()
 
 
-def no(mf: MF) -> MF:
+def no(mf: Mf) -> Mf:
     '''Invert the result of a monadic computation, AKA negation as failure.'''
     def _(v: Value):
         def __(c: Cont):
@@ -53,36 +52,36 @@ def no(mf: MF) -> MF:
     return _
 
 
-def then(mf: MF, mg: MF) -> MF:
+def then(mf: Mf, mg: Mf) -> Mf:
     '''Apply two monadic functions mf and mg in sequence.
     Together with 'unit', this makes the monad also a monoid.'''
     return lambda v: bind(mf(v), mg)
 
 
-def _alt_from_iterable(mfs: Sequence[MF]) -> MF:
+def _alt_from_iterable(mfs: Sequence[Mf]) -> Mf:
     '''Find solutions matching any one of mfs.'''
     return lambda v: lambda c: chain.from_iterable(mf(v)(c) for mf in mfs)
 
 
-def alt(*mfs: MF) -> MF:
+def alt(*mfs: Mf) -> Mf:
     '''Find solutions matching any one of mfs.'''
     return _alt_from_iterable(mfs)
 
 alt.from_iterable = _alt_from_iterable  # type: ignore
 
 
-def _seq_from_iterable(mfs: Sequence[MF]) -> MF:
+def _seq_from_iterable(mfs: Sequence[Mf]) -> Mf:
     '''Find solutions matching all mfs.'''
     return reduce(then, mfs, unit)  # type: ignore
 
 
-def seq(*mfs: MF) -> MF:
+def seq(*mfs: Mf) -> Mf:
     '''Find solutions matching all mfs.'''
     return _seq_from_iterable(mfs)
 
 seq.from_iterable = _seq_from_iterable  # type: ignore
 
 
-def run(ma: MA) -> Result:
+def run(ma: Ma) -> Result:
     '''Start the monadic computation of ma.'''
     return ma(lambda v: (yield v))  # type: ignore
