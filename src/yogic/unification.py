addd956aecb2f55210668ff615da568ceba96dd7
Mick Krippendorf <m.krippendorf@freenet.de>
2023-07-04 15:04:40 +0200

    fixed typo in docstring

--- src/yogic/unification.py
+++ src/yogic/unification.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021 Mick Krippendorf <m.krippendorf@freenet.de>
 
-'''A unification mechanism where variable get bout to values.'''
+'''A unification mechanism where variables get bout to values.'''
 
 
 from collections import ChainMap
