4ff5db3a925d054e0341e11f9310b96cf809d981
Mick Krippendorf <m.krippendorf@freenet.de>
2023-07-19 22:39:58 +0200

    renamed a closure.

--- src/yogic/backtracking.py
+++ src/yogic/backtracking.py
@@ -43,9 +43,9 @@ def failure() -> Solutions:
 def bind(ma:Ma, mf:Mf) -> Ma:
     '''Return the result of applying mf to ma.'''
     def bound(y:Success, n:Failure, e:Escape) -> Solutions:
-        def yes(v:Value, m:Failure) -> Solutions:
+        def on_success(v:Value, m:Failure) -> Solutions:
             return mf(v)(y, m, e)
-        return ma(yes, n, e)
+        return ma(on_success, n, e)
     return bound
 
 
