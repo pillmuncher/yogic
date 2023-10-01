758c6144d0b7f2eddc70db7ec975a843a7fa6f95
pillmuncher <m.krippendorf@freenet.de>
2023-10-01 16:04:24 +0200

    version bump machinery finally works?

--- src/yogic/__init__.py
+++ src/yogic/__init__.py
@@ -41,7 +41,7 @@
 __date__ = '2023-07-04'
 __author__ = 'Mick Krippendorf <m.krippendorf@freenet.de>'
 __license__ = 'MIT'
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 __all__ = (
     # re-export from .backtracking
