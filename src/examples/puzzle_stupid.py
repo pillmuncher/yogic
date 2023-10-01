89f0e6e86c2b1083eacd2911937fa421024d2386
pillmuncher <m.krippendorf@freenet.de>
2023-10-01 20:31:37 +0200

    cleaned up stupid puzzle. cleaned up Pipfile.

--- src/examples/puzzle_stupid.py
+++ src/examples/puzzle_stupid.py
@@ -17,11 +17,12 @@ def equate(letters, numbers):
 a, b, c, d, e, f, g, h, i, j, k, l = (var() for _ in range(12))
 
 puzzle = seq(
-    equate([a, d, e, f, j, k, l], [2, 8, 6, 9, 1, 7, 4]),
-    equate([a, b, c, e, h, i, j], [11, 1, 12, 8, 4, 5, 10]),
-    equate([b, c, d, e, f, g, h], [5, 2, 9, 8, 11, 3, 10]),
-    equate([a, d, f, g, i, j, k], [4, 6, 12, 1, 2, 9, 3]),
-    equate([b, c, g, h, j, l], [3, 4, 7, 11, 5, 10]),
+    equate({a, b, c, e, h, i, j}, {2, 4, 5, 8, 10, 11, 12}),
+    equate({a, b, f, i, j, k, l}, {1, 4, 5, 6, 7, 8, 12}),
+    equate({a, c, d, e, f, k, l}, {1, 2, 6, 7, 8, 9, 10}),
+    equate({a, c, f, g, i, j, k}, {1, 2, 3, 4, 6, 8, 12}),
+    equate({b, c, d, e, f, g, h}, {1, 2, 3, 5, 9, 10, 11}),
+    equate({b, c, e, g, h, j, l}, {2, 3, 4, 5, 7, 10, 11}),
 )
 
 for n, each in enumerate(resolve(puzzle)):
